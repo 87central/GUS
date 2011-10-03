5170c398889dde414507a934d9c6415762a2132a
Mike Naughton <mike@fundle.co>
2011-10-03 11:13:11 -0500

    Finished the preliminary version of the job dashboard by creating the job table.

--- protected/views/job/dashboard.php
+++ protected/views/job/dashboard.php
@@ -8,6 +8,46 @@ Welcome <?php echo Yii::app()->user->name;?>&nbsp;
 <br/>
 <!--table goes here-->
 <?php 
+$this->widget('zii.widgets.grid.CGridView', array(
+	'dataProvider'=>new CActiveDataProvider($jobs),
+	'formatter'=>new CFormatter,
+	'columns'=>array(
+		array(
+			'name'=>'pickUpDate',
+			'value'=>"date('l', \$data->pickUpDate);",
+			'header'=>'Pick-Up',
+		),
+		array(
+			'header'=>'Open Jobs',
+			'value'=>"\$data->RUSH ? '<span class=\"warning\">RUSH</span>&nbsp;' : '' . \$data->DESCRIPTION",
+		),
+		array(
+			'header'=>'Status',
+			'value'=>'',
+		),
+		array(
+			'header'=>'Print',
+			'name'=>'printDate',
+			'value'=>"date('l', \$data->printDate);",
+		),
+		array(
+			'header'=>'Due',
+			'name'=>'dueDate',
+			'value'=>"date('n/j', \$data->dueDate);"
+		),
+		'totalPasses',
+		array(
+			'header'=>'Art',
+			'value'=>"CHtml::image(Yii::app()->request->baseUrl . '/images/' . \$data->hasArt ? 'checked.png' : 'unchecked.png');",
+		),
+		array(
+			'header'=>'Art',
+			'value'=>"CHtml::image(Yii::app()->request->baseUrl . '/images/' . \$data->hasSizes ? 'checked.png' : 'unchecked.png');",
+		)
+	)
+));
+?>
+<?php 
 $this->widget('application.components.Menu', array(
 	'items'=>array(
 		array('label'=>'+ New Job', 'url'=>array('job/create')),
