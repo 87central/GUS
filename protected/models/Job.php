3b67195f093a49c810a44eea70b6f2ab1be30f2a
Mike Naughton <mike@fundle.co>
2011-10-03 11:11:51 -0500

    Added events relation and several simple getters. Removed the dueDate functions and created a more flexible event attribute retrieval and update mechanism.

--- protected/models/Job.php
+++ protected/models/Job.php
@@ -22,7 +22,6 @@
  */
 class Job extends CActiveRecord
 {
-	private $_dueEvent;
 	/**
 	 * Returns the static model of the specified AR class.
 	 * @return Job the static model class
@@ -70,6 +69,7 @@ class Job extends CActiveRecord
 			'lEADER' => array(self::BELONGS_TO, 'User', 'LEADER_ID'),
 			'jobLines' => array(self::HAS_MANY, 'JobLine', 'JOB_ID'),			
 			'printJob' => array(self::BELONGS_TO, 'PrintJob', 'PRINT_ID'),
+			'events'=> array(self::HAS_MANY, 'EventLog', 'OBJECT_ID', 'condition'=>'OBJECT_TYPE = \'Job\'', 'index'=>'EVENT_ID'),
 		);
 	}
 
@@ -89,7 +89,97 @@ class Job extends CActiveRecord
 			'SET_UP_FEE' => 'Set Up Fee',
 			'SCORE' => 'Score',
 			'QUOTE' => 'Quote',
+			'totalPasses' => 'Passes',
 		);
+	}
+	
+	/**
+	 * Gets an array mapping attribute names to event IDs.
+	 * @return array The resultant array.
+	 */
+	protected function eventAttributes(){
+		return array(
+			'dueDate'=>EventLog::JOB_DUE,
+			'printDate'=>EventLog::JOB_PRINT,
+			'pickUpDate'=>EventLog::JOB_PICKUP,
+		);
+	}
+	
+	public function __get($name){
+		$found = false;
+		$originalName = $name;
+		//first, determine if client code is requesting a "formatted" attribute
+		if(($pos = strpos('formatted', $name)) === 0){
+			$formatted = true;
+			$name = substr($name, 9); //9 is length of "formatted"
+			$first = substr($name, 0, 1); //get first character
+			$first = strtolower($first);
+			$name = substr_replace($name, $first, 0, 1);
+		} else {
+			$formatted = false;
+		}
+		
+		//then, if the (unformatted) attribute is an event attribute,
+		//get the event value
+		foreach($this->eventAttributes() as $attrName => $eventID){
+			if(strcmp($name, $attrName) == 0){
+				$event = $this->getEventModel($eventID);
+				if($formatted){
+					if($event->DATE !== null){
+						$formatter = new CFormatter;
+						$value = $formatter->formatDate($event->DATE);
+					} else {
+						$value = null;
+					}
+				} else {
+					$value = $event->DATE;
+				}
+				$found = true;
+			}
+		}
+		
+		//if we found it, return it, otherwise, return what the parent thinks 
+		//is a matching attribute 
+		if(!$found){
+			return parent::__get($name);
+		} else {
+			return $value;
+		}
+	}
+	
+	public function __set($name, $value){
+		$found = false;
+		$originalName = $name;
+		//first, determine if client code is requesting a "formatted" attribute
+		if(($pos = strpos('formatted', $name)) === 0){
+			$formatted = true;
+			$name = substr($name, 9); //9 is length of "formatted"
+			$first = substr($name, 0, 1); //get first character
+			$first = strtolower($first);
+			$name = substr_replace($name, $first, 0, 1);
+		} else {
+			$formatted = false;
+		}
+		
+		//then, if the (unformatted) attribute is an event attribute,
+		//set the event value
+		foreach($this->eventAttributes() as $attrName => $eventID){
+			if(strcmp($name, $attrName) == 0){
+				$event = $this->getEventModel($eventID);
+				if($formatted){
+					$event->DATE = strtotime($value);
+				} else {
+					$event->DATE = $value;
+				}
+				$found = true;
+			}
+		}
+		
+		//if we found it, set it, otherwise, set what the parent thinks 
+		//is a matching attribute 
+		if(!$found){
+			parent::__set($name, $value);
+		}
 	}
 
 	/**
@@ -119,51 +209,44 @@ class Job extends CActiveRecord
 		));
 	}
 	
-	protected function getDueDateModel(){
-		if($this->_dueEvent === null){
-			$event = EventLog::model()->findByAttributes(array(
-				'OBJECT_TYPE'=>'Job',
-				'OBJECT_ID'=>$this->ID,
-				'EVENT_ID'=>EventLog::JOB_DUE,
-			));
-			
-			if($event === null){
-				$event = new EventLog;
-				$event->assocObject = $this;
-				$event->EVENT_ID = EventLog::JOB_DUE; 
-			}
-			$this->_dueEvent = $event;
-		}
-		return $this->_dueEvent;
-	}
-	
-	public function getDueDate(){
-		$event = $this->dueDateModel;
-		return $event->DATE;
-	}
-	
-	public function setDueDate($value){
-		$this->dueDateModel->DATE = $value;
-	}
-	
-	public function getFormattedDueDate(){
-		if($this->dueDate == null){
-			return null;
+	protected function getEventModel($eventID){
+		if(!isset($this->events[$eventID])){
+			$event = new EventLog;
+			$event->assocObject = $this;
+			$event->EVENT_ID = $eventID;
+			$this->events[$eventID] = $event;
 		} else {
-			$formatter = new CFormatter;
-			return $formatter->formatDate($this->dueDate);
+			$event = $this->events[$eventID];
 		}
-	}
-	
-	public function setFormattedDueDate($value){
-		$this->dueDate = strtotime($value);
+		return $event;
 	}
 	
 	protected function afterSave(){
 		parent::afterSave();
-		if($this->_dueEvent !== null){
-			$this->_dueEvent->OBJECT_ID = $this->ID;
-			$this->_dueEvent->save();
+		if(isset($this->events)){
+			foreach($this->events as $event){
+				$event->OBJECT_ID = $this->ID;
+				$event->save();
+			}
 		}
+	}
+	
+	public function getTotalPasses(){
+		return count($this->jobLines) * $this->printJob->PASS;
+	}
+	
+	public function getHasArt(){
+		$result = false;
+		if($this->printJob !== null){
+			if($this->printJob->ART != null){
+				$result = true;
+			}
+		}
+		return $result;
+	}
+	
+	//this was in the mockup, but I'm not quite sure what it's for!
+	public function getHasSizes(){
+		return true;
 	}
 }
