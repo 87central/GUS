5d87d501f014975258b695a8d530b70ae7f747f7
Mike Naughton <mike@fundle.co>
2011-10-03 11:12:28 -0500

    Removed some code that was rendered unnecessary by a change to the job object.

--- protected/views/job/_eventDetail.php
+++ protected/views/job/_eventDetail.php
@@ -1,12 +1,7 @@
 <?php 
 	$job = $item->getObject();
-	$passes = 0;
-	foreach($job->jobLines as $jobLine){
-		$print = $jobLine->print;
-		$passes = $passes + $print->PASS;
-	}
 ?>
 <?php if($job->RUSH){?>
 	<span class="warning">RUSH</span>&nbsp;
 <?php } ?>
-<?php echo CHtml::encode($job->DESCRIPTION);?>&nbsp;(<?php echo $passes;?>)
+<?php echo CHtml::encode($job->DESCRIPTION);?>&nbsp;(<?php echo $job->totalPasses;?>)
