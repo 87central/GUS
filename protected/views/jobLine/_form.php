80b7b5cadd4c2e83643cc11b9ac48db70555cf0e
Mike Naughton <mike@fundle.co>
2011-10-04 17:08:38 -0500

    Removed some unneeded calculations.

--- protected/views/jobLine/_form.php
+++ protected/views/jobLine/_form.php
@@ -1,20 +1,20 @@
-<?php 
-$sizeList = CHtml::listData($sizes, 'ID', 'TEXT');
-$styleList = CHtml::listData($styles, 'ID', 'TEXT');
-$colorList = CHtml::listData($colors, 'ID', 'TEXT');
-?>
-Style: <?php echo CHtml::activeDropDownList($model, 'STYLE', $styleList, array(
-	'name'=>$namePrefix . '[STYLE]',
-));?>
-Size: <?php echo CHtml::activeDropDownList($model, 'SIZE', $sizeList, array(
-	'name'=>$namePrefix . '[SIZE]',
-));?>
-Color: <?php echo CHtml::activeDropDownList($model, 'COLOR', $colorList, array(
-	'name'=>$namePrefix . '[COLOR]',
-));?>
-Quantity: <?php echo CHtml::activeTextField($model, 'QUANTITY', array(
-	'name'=>$namePrefix . '[QUANTITY]',
-));?>
-<?php echo CHtml::activeHiddenField($model, 'ID', array(
-	'name'=>$namePrefix . '[ID]',
-));?>
+<div class="jobLine">
+	Style: <?php echo CHtml::activeDropDownList($model, 'style', $styles, array(
+		'name'=>$namePrefix . '[style]',
+	));?>
+	Size: <?php echo CHtml::activeDropDownList($model, 'size', $sizes, array(
+		'name'=>$namePrefix . '[size]',
+	));?>
+	Color: <?php echo CHtml::activeDropDownList($model, 'color', $colors, array(
+		'name'=>$namePrefix . '[color]',
+	));?>
+	Quantity: <?php echo CHtml::activeTextField($model, 'QUANTITY', array(
+		'name'=>$namePrefix . '[QUANTITY]',
+	));?>
+	Price: <?php echo CHtml::activeTextField($model, 'PRICE', array(
+		'name'=>$namePrefix . '[PRICE]',
+	))?>
+	<?php echo CHtml::activeHiddenField($model, 'ID', array(
+		'name'=>$namePrefix . '[ID]',
+	));?>
+</div>
