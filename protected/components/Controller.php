a2f8bd756ec77f2e8827dc172dc5aa0b54c7b1ad
Mike Naughton <mike@fundle.co>
2011-10-06 19:32:31 -0500

    Changed the base controller to a version which provides shortcuts to several directories.

--- protected/components/Controller.php
+++ protected/components/Controller.php
@@ -5,11 +5,25 @@
  */
 class Controller extends CController
 {
+	private $_layout = 'column1';
+				
+	public function getLayout(){
+		return $this->layoutDirectory . $this->layoutCore;
+	}
+	
 	/**
-	 * @var string the default layout for the controller view. Defaults to '//layouts/column1',
-	 * meaning using a single column layout. See 'protected/views/layouts/column1.php'.
+	 * Gets the layout name (the very last path segment in the layout path).
+	 * @return String The layout name.
 	 */
-	public $layout='//layouts/column1';
+	protected function getLayoutCore(){
+		return $this->_layout;
+	}
+	
+	public function setLayout($layout){
+		$this->_layout = $layout;
+		$this->layout = $this->getLayout();
+	}
+	
 	/**
 	 * @var array context menu items. This property will be assigned to {@link CMenu::items}.
 	 */
@@ -20,4 +34,70 @@ class Controller extends CController
 	 * for more details on how to specify this property.
 	 */
 	public $breadcrumbs=array();
+	/**
+	 * @var array Extended context menu items.
+	 */
+	public $pageOperations = array();
+	/**
+	 * Indicates whether the current browser is a "mobile" browser instance.
+	 */
+	public function getIsMobile(){
+		/*$cookies = Yii::app()->request->cookies;
+		$isMobile = Yii::app()->browser->isMobile();
+		if(isset($cookies['SUPPRESS_MOBILE']) && $cookies['SUPPRESS_MOBILE']->value == 1){
+			$isMobile = false;
+		}
+		//return true;
+		$isMobile = false; //pending redesign of mobile site
+		return $isMobile;*/
+		return false;
+	}
+	
+	protected function beforeAction($action){
+		if(parent::beforeAction($action)){
+			$this->setLayout('column1');
+			if(isset($this->actionParams['mobile'])){
+				$cookies = Yii::app()->request->cookies;
+				$cookies['SUPPRESS_MOBILE'] = $this->createCookie('SUPPRESS_MOBILE', $this->actionParams['mobile']);
+			}
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	/**
+	 * Creates a cookie. 
+	 * @param string $name The name of the cookie.
+	 * @param string $value The value of the cookie.
+	 * @return CHttpCookie The cookie instance.
+	 */
+	private function createCookie($name, $value){
+		$cookie = new CHttpCookie($name, $value);
+		$cookie->secure = false;
+		$cookie->httpOnly = true;
+		return $cookie;
+	}
+	
+	protected function getLayoutDirectory(){
+		$directory = '//layouts/desktop/';
+		if($this->isMobile){
+			$directory = '//layouts/mobile/';
+		}
+		$directory = '//layouts/';
+		return $directory;
+	}
+	
+	public function getStyleDirectory(){
+		$directory = '/css/';
+		if($this->isMobile){
+			$directory = '/css/mobile/';
+		}
+		$directory = Yii::app()->request->baseUrl . $directory;
+		return $directory;
+	}
+	
+	public function getScriptDirectory(){
+		return Yii::app()->request->baseUrl . '/assets/';
+	}
 }
