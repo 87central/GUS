a8a4b7cbb7fdcc128cf5ab743e24c3b39b9a61d5
Mike Naughton <mike@fundle.co>
2011-10-09 13:33:22 -0500

    Added buttons and "live" event listening to support approval, unapproval, and deletion of job lines.

--- protected/views/jobLine/_form.php
+++ protected/views/jobLine/_form.php
@@ -2,8 +2,9 @@
 <?php $total = '#'.CHtml::getIdByName($namePrefix);?>
 <?php $qty = '#'.CHtml::getIdByName($namePrefix . '[QUANTITY]');?>
 <?php $price = '#'.CHtml::getIdByName($namePrefix . '[PRICE]');?>
+<?php $div = CHtml::getIdByName($namePrefix . 'item');?>
 
-<div class="jobLine">
+<div class="jobLine" id="<?php echo $div;?>">
 	Style: <?php echo CHtml::activeDropDownList($model, 'style', $styles, array(
 		'name'=>$namePrefix . '[style]',
 	));?>
@@ -30,4 +31,70 @@
 		'id'=>CHtml::getIdByName($namePrefix),
 		'readonly'=>'readonly',
 	));?>
+	<?php echo CHtml::hiddenField('prefix', $namePrefix, array(
+		'class'=>'namePrefix',
+	));?>
+	
+	<?php if(!$model->isApproved){?>
+		<?php echo CHtml::button('Approve', array(
+			'class'=>'line_approve',
+		));?>		
+	<?php } else {?>
+		<?php echo CHtml::button('Unapprove', array(
+			'class'=>'line_unapprove',
+		));?>
+	<?php }?>
+	
+	<?php echo CHtml::button('Delete', array(
+		'class'=>'line_delete',
+	));?>
 </div>
+
+<?php Yii::app()->clientScript->registerScript('line-approve', "" .
+		"$('.line_approve').live('click', function(event){
+			var div = $(event.target).parent();" .
+			"$.ajax({
+				'url': '".CHtml::normalizeUrl(array('job/approveLine'))."'," .
+				"'type': 'POST'," .
+				"'data': {
+					'id': '".$model->ID."'," .
+					"'namePrefix': $(div).children('.namePrefix').val(),
+				}," .
+				"'success': function(data){
+					$(div).replaceWith(data);
+				},
+			});
+		})", 
+CClientScript::POS_END);?>
+<?php Yii::app()->clientScript->registerScript('line-unapprove', "" .
+		"$('.line_unapprove').live('click', function(event){
+			var div = $(event.target).parent();" .
+			"$.ajax({
+				'url': '".CHtml::normalizeUrl(array('job/unapproveLine'))."'," .
+				"'type': 'POST'," .
+				"'data': {
+					'id': '".$model->ID."'," .
+					"'namePrefix': $(div).children('.namePrefix').val(),
+				}," .
+				"'success': function(data){
+					$(div).replaceWith(data);
+				},
+			});
+		})", 
+CClientScript::POS_END);?>
+<?php Yii::app()->clientScript->registerScript('line-delete', "" .
+		"$('.line_delete').live('click', function(event){
+			var div = $(event.target).parent();" .
+			"$.ajax({
+				'url': '".CHtml::normalizeUrl(array('job/deleteLine'))."'," .
+				"'type': 'POST'," .
+				"'data': {
+					'id': '".$model->ID."'," .
+					"'namePrefix': $(div).children('.namePrefix').val(),
+				}," .
+				"'success': function(data){
+					$(div).remove();
+				},
+			});
+		})", 
+CClientScript::POS_END);?>
