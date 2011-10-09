480b28c9adbb89e7d1481e4cd75aaae1ec75cc66
Mike Naughton <mike@fundle.co>
2011-10-09 09:44:06 -0500

    Created a div around the job event view and added necessary code to adjust it's height based on the job score.

--- protected/views/job/_eventDetail.php
+++ protected/views/job/_eventDetail.php
@@ -1,8 +1,12 @@
-<?php 
-	$job = $item->getAssocObject();
-?>
-<?php if($job->RUSH){?>
-	<span class="warning">RUSH</span>&nbsp;
-<?php } ?>
-<?php echo CHtml::encode($job->DESCRIPTION);?>&nbsp;(<?php echo $job->totalPasses;?>)
-<?php echo CHtml::activeHiddenField($item, 'ID');?>
+<?php $id = 'job_evt_'.$job->ID.$item->ID;?>
+<div id="<?php echo $id;?>">
+	<?php 
+		$job = $item->getAssocObject();
+	?>
+	<?php if($job->RUSH){?>
+		<span class="warning">RUSH</span>&nbsp;
+	<?php } ?>
+	<?php echo CHtml::encode($job->NAME);?>&nbsp;(<?php echo $job->score;?>)
+	<?php echo CHtml::activeHiddenField($item, 'ID');?>
+	<?php Yii::app()->clientScript->registerCss($id, "#$id{height:".($job->score / 480 *  10)."em}");/*480 is number of minutes in 8 hours*/?>
+</div>
