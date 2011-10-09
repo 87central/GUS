a2a5511c28d7b07777cef3bbcda0b9a2e5cb79d5
Mike Naughton <mike@fundle.co>
2011-10-09 14:45:13 -0500

    Added a way to access the editor for lookups.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -62,6 +62,7 @@
 								array('label'=>'View Products', 'url'=>array('/product/index')),
 								array('label'=>'View Vendors', 'url'=>array('/vendor/index')),
 								array('label'=>'View Customers', 'url'=>array('/customer/index')),
+								array('label'=>'Colors, Etc.', 'url'=>array('/lookup/index', 'Color'=>1, 'Style'=>1, 'Size'=>1))
 							),
 							'lastItemCssClass'=>'lastmenu',
 						));
