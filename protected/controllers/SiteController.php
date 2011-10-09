20fe402b71d3f19144a6cf2a4f27be0e0fa76eb2
Mike Naughton <mike@fundle.co>
2011-10-09 13:22:43 -0500

    The site controller now automatically redirects to the job index on successful login.

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -86,7 +86,7 @@ class SiteController extends Controller
 			$model->attributes=$_POST['LoginForm'];
 			// validate user input and redirect to the previous page if valid
 			if($model->validate() && $model->login())
-				$this->redirect(Yii::app()->user->returnUrl);
+				$this->redirect(array('job/index'));//$this->redirect(Yii::app()->user->returnUrl);
 		}
 		// display the login form
 		$this->render('login',array('model'=>$model));
