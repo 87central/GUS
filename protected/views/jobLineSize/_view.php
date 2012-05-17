bdd07f56e974e4c5f8dfa691901538de0f568245
Mike Naughton <mike@fundle.co>
2012-05-16 21:40:38 -0500

    Some fixes to the job line size view, and added the estimate and view views for job line size.

---
+++ protected/views/jobLineSize/_view.php
@@ -0,0 +1,4 @@
+<div class="jobLine <?php echo ($product->ID == null) ? 'hidden-size' : '';?> <?php echo $div.$product->SIZE;?>" id="<?php echo $eachDiv;?>">
+	<?php echo CHtml::label($product->size->TEXT, CHtml::getIdByName($linePrefix . '[QUANTITY]'));?>
+	<?php echo CHtml::encode($line->QUANTITY);?>
+</div>
