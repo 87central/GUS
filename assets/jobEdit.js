e1cd3cfffec6801b792cbdfe115f6cb228755e3b
Mike Naughton <mike@fundle.co>
2012-05-17 16:07:01 -0500

    Added code to help calculate the setup fee. Moved the garment count calculation to its own function. Added functions to decide what to do when the garment cost changes. Added a function which can be called by the various style boxes when their value changes.

--- assets/jobEdit.js
+++ assets/jobEdit.js
@@ -10,8 +10,26 @@ function calculateTotalCore(url, garments, front, back, sleeve, completion){
 	);
 }
 
+function calculateSetupFeeCore(url, garments, front, back, sleeve, completion){
+	$.getJSON(url,
+	{
+		garments: garments,
+		front: front,
+		back: back,
+		sleeve: sleeve,
+	},
+	completion
+	);
+}
+
 function calculateTotalMain(url, garments, front, back, sleeve, dest){
 	calculateTotalCore(url, garments, front, back, sleeve, function(data){
+		$(dest).val(data.result).change();
+	});
+}
+
+function calculateSetupFeeMain(url, garments, front, back, sleeve, dest){
+	calculateSetupFeeCore(url, garments, front, back, sleeve, function(data){
 		$(dest).val(data.result).change();
 	});
 }
@@ -28,6 +46,14 @@ function getSleevePasses(){
 	return $('.sleeve_pass').val();
 }
 
+function getGarmentCount(estimate){
+	var garmentCount = 0;
+	$(estimate).parentsUntil('.jobLines').parent().find('.item_qty').each(function(){
+		garmentCount += 1 * $(this).val();
+	});
+	return garmentCount;
+}
+
 function refreshEstimate(editVal, estimateVal, estimate){	
 	$(estimate).children('span').html(estimateVal);
 	$(estimate).children(':hidden').val(estimateVal);
@@ -38,34 +64,94 @@ function refreshEstimate(editVal, estimateVal, estimate){
 	}
 }
 
-function updateLineTotal(calculatorUrl, editable, estimate, total){
+function updateLineTotal(calculatorUrl, editable, estimate, total, cost){
 	var editVal = $(editable).val() * 1;
+	var costVal = $(cost).val() * 1;
 	var totalVal = 0;
-	var garmentCount = 0;
+	var garmentCount = getGarmentCount(estimate);
 	var estimateVal = 0;
-	$(estimate).parent().parent().children('.jobLine').children('.item_qty').each(function(){
-		garmentCount += 1 * $(this).val();
-	});
 	calculateTotalCore(calculatorUrl, garmentCount, getFrontPasses(), getBackPasses(), getSleevePasses(), function(data){
 		var oldEstimate = $(estimate).children(':hidden').val() * 1;
 		if(oldEstimate == editVal){
-			editVal = 1 * data.result / garmentCount
+			editVal = 1 * data.result / garmentCount + costVal;
 			$(editable).val(editVal);
 		}
-		refreshEstimate(editVal, 1 * data.result / garmentCount, estimate);
+		refreshEstimate(editVal, 1 * data.result / garmentCount + costVal, estimate);
 		$(total).val(editVal * garmentCount).change();
 	});
 	$(total).val(editVal * garmentCount).change();
 }
 
-function recalculateTotal(editable, estimate, total){
+function recalculateTotal(editable, estimate, total, cost){
 	var editVal = $(editable).val() * 1;
-	var garmentCount = 0;
-	$(estimate).parent().parent().children('.jobLine').children('.item_qty').each(function(){
-		garmentCount += 1 * $(this).val();
-	});
+	var garmentCount = getGarmentCount(estimate);
 	refreshEstimate(editVal, $(estimate).children(':hidden').val(), estimate);
 	$(total).val(editVal * garmentCount).change();
+}
+
+function onGarmentCostUpdate(costField, newCost, editable, estimate, total){
+	var oldCost = $(costField).val() * 1;
+	var garmentCount = getGarmentCount(estimate);
+	var oldEstimate = $(estimate).val() * 1;
+	var newEstimate = oldEstimate + 1 * newCost - oldCost;
+	var editVal = $(editable).val() * 1;
+	if(oldCost != newCost){
+		if(oldEstimate == editVal){
+			editVal = newEstimate;
+			$(editable).val(editVal);
+		}
+		refreshEstimate(editVal, newEstimate, $(estimate).parent());
+		$(costField).val(newCost);
+		$(total).val(editVal * garmentCount).change();		
+	}
+}
+
+function createStyleSelectFunction(div_id, style_id){
+	return function(data){
+		var colors = data.colors;
+		var sizes = data.sizes;
+		var cost = data.productCost;
+		var colorOptions = $('<select></select>')
+			.attr('name', 'color-select')
+			.attr('class', 'color-select');
+		for(var color in colors){
+			colorOptions.append($('<option></option>').val(colors[color].ID).html(colors[color].TEXT));
+		}
+		colorOptions.attr('name', $('#' + div_id).children('.color-select').attr('name'));
+		$('#' + div_id).children('.color-select').replaceWith(colorOptions);
+		$('#' + div_id).children('.jobLine').addClass('hidden-size').children('.score_part').attr('disabled', true).val(0);
+		$('#' + div_id).children('.jobLine').children('.hidden_cost').val(cost);
+		onGarmentCostUpdate($('#' + div_id).find('.product-cost'), cost, $('#' + div_id).find('.editable-price'), $('#' + div_id).find('.hidden-price'), $('#' + div_id).find('.garment_part'));
+		for(var size in sizes){
+			$('#' + div_id).children('.' + div_id + sizes[size].ID)
+			.removeClass('hidden-size')
+			.parent().children('.score_part').removeAttr('disabled');
+		}
+		$('#' + div_id).children('.hidden-style').val(style_id);
+	}
+}
+
+function refreshSetupFee(editVal, feeVal, hidden){
+	$(hidden).children('span').html(feeVal);
+	$(hidden).children(':hidden').val(feeVal);
+	if(feeVal == editVal){
+		$(hidden).hide();
+	} else {
+		$(hidden).show();
+	}
+}
+
+function updateSetupCost(url, editable, hidden, garmentCount){
+	var oldCost = $(hidden).children(':hidden').val() * 1;
+	var editVal = $(editable).val() * 1;
+	calculateSetupFeeCore(url, garmentCount, getFrontPasses(), getBackPasses(), getSleevePasses(), function(data){
+		var newCost = data.result;
+		if(oldCost == editVal){
+			editVal = newCost;
+			$(editable).val(editVal).change();
+		}
+		refreshSetupFee(editVal, newCost, hidden);
+	});
 }
 
 function preprocessLine(form, line){
