306034594df71733e69022edaebe4a5aefd0332f
Mike Naughton <mike@fundle.co>
2012-05-19 10:27:57 -0500

    Corrected incorrect variable reference.

--- protected/views/jobLineSize/_view.php
+++ protected/views/jobLineSize/_view.php
@@ -1,4 +1,4 @@
-<div class="jobLine <?php echo ($product->JOB_LINE_ID == null) ? 'hidden-size' : '';?> <?php echo $div.$product->SIZE;?>" id="<?php echo $eachDiv;?>">
+<div class="jobLine <?php echo ($line->JOB_LINE_ID == null) ? 'hidden-size' : '';?> <?php echo $div.$product->SIZE;?>" id="<?php echo $eachDiv;?>">
 	<?php echo CHtml::label($product->size->TEXT, CHtml::getIdByName($linePrefix . '[QUANTITY]'));?>
 	<?php echo CHtml::encode($line->QUANTITY);?>
 </div>
