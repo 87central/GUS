ff6229bd44c05a9228284ec4d0d5dcd773070779
Mike Naughton <mike@fundle.co>
2012-06-14 15:35:04 -0500

    Second time is a charm...

--- assets/jobOperations.js
+++ assets/jobOperations.js
@@ -64,24 +64,6 @@ function refreshEstimate(editVal, estimateVal, estimate){
 	}
 }
 
-function updateLineTotal(calculatorUrl, editable, estimate, total, cost){
-	var editVal = $(editable).val() * 1;
-	var costVal = $(cost).val() * 1;
-	var totalVal = 0;
-	var garmentCount = getGarmentCount(estimate);
-	var estimateVal = 0;
-	calculateTotalCore(calculatorUrl, garmentCount, getFrontPasses(), getBackPasses(), getSleevePasses(), function(data){
-		var oldEstimate = $(estimate).children('.hidden-price').val() * 1;
-		if(oldEstimate == editVal){
-			editVal = 1 * data.result / garmentCount + costVal;
-			$(editable).val(editVal);
-		}
-		refreshEstimate(editVal, 1 * data.result / garmentCount + costVal, estimate);
-		$(total).val(editVal * garmentCount).change();
-	});
-	$(total).val(editVal * garmentCount).change();
-}
-
 function recalculateTotal(editable, estimate, total, cost){
 	var editVal = $(editable).val() * 1;
 	var garmentCount = getGarmentCount(estimate);
